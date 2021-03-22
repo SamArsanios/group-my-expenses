df3c470b1f065be1f0fc82e858c1add2ea1c67dd
SamArsanios <samson.kibrom@hotmail.com>
2021-03-22 08:06:58 +0300

    Create Expene model and Migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_050147) do
+ActiveRecord::Schema.define(version: 2021_03_22_050547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
