2d68c96caf1ee3b55db33418aede9f04acb46695
SamArsanios <samson.kibrom@hotmail.com>
2021-03-22 17:09:17 +0300

    Add Login and Logout sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new; end
+
+    def create
+        @user = User.find_by(name: params[:session][:name])
+        if @user&.authenticate(params[:session])
+        log_in @user
+        flash[:success] = ['Successful Login']
+        redirect_to @user
+        else
+        flash.now[:danger] = ["User doesn't exist"]
+        render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_url
+    end
 end
