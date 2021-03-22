ca36de31f2b2b7fe04f56beac5e43180d1b7b8bb
SamArsanios <samson.kibrom@hotmail.com>
2021-03-23 02:10:37 +0300

    Add route to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
   resources :users, only: %i[new create show]
   resources :expenses, only: %i[index new create]
   resources :groups, only: %i[index new create show]
