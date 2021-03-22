77e9cf385a838d2f0eeec5f82ba4c54c74d8c4e0
SamArsanios <samson.kibrom@hotmail.com>
2021-03-22 11:31:21 +0300

    Add show page and helper method for show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    def display_avatar(user)
+        if user.avatar.attached?
+          image_tag user.display_image, class: 'avatar rounded-circle'
+        else
+          image_tag 'default-avatar.png', class: 'avatar rounded-circle'
+        end
+    end
 end
