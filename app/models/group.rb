6f018ea6155657d8c70ba47b4278bad37909eacd
SamArsanios <samson.kibrom@hotmail.com>
2021-03-22 18:10:06 +0300

    Add associations and validations to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,14 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :expenses_groups, dependent: :destroy
+  has_many :expenses, through: :expenses_groups
+  
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
+  has_one_attached :icon, dependent: :destroy
+  validates :icon, blob: { content_type: :image }
+
+  # Returns resized image
+  def display_image
+    icon.variant(resize_to_limit: [500, 500])
+  end
 end
