82450488b6939833bc0459a90f575174a219f2be
SamArsanios <samson.kibrom@hotmail.com>
2021-03-22 08:28:38 +0300

    Create groups model and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_050547) do
+ActiveRecord::Schema.define(version: 2021_03_22_052129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_03_22_050547) do
     t.index ["author_id"], name: "index_expenses_on_author_id"
   end
 
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -31,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_03_22_050547) do
     t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "groups", "users"
 end
