f7901ca1c87fe4badf150fe04339324fe62edaa0
SamArsanios <samson.kibrom@hotmail.com>
2021-03-22 08:46:07 +0300

    Add user associations and validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_many :expenses, foreign_key: 'author_id', dependent: :destroy
+    has_many :groups, dependent: :destroy
+    validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 }
+    has_one_attached :avatar, dependent: :destroy
+    validates :avatar, blob: { content_type: :image }
+  
+    # Returns resized image
+    def display_image
+      avatar.variant(resize_to_limit: [500, 500])
+    end
 end
