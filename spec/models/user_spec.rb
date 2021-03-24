e2ed08bc763d02ea441a8fb0a42ac4fd63d20474
SamArsanios <samson.kibrom@hotmail.com>
2021-03-24 09:47:16 +0300

    Modify user test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'validates user if length of name is >= 3' do
-    user = User.new(name: 'test_user')
+    user = User.new(name: 'user1')
     expect(user.valid?).to be true
   end
 
   it "doesn't validate user if length of name is < 3" do
-    user = User.new(name: 'tt')
+    user = User.new(name: 'jo')
     expect(user.valid?).to be false
   end
 
-  it "doesn't validate user if name already exists" do
-    User.create!(name: 'test_user')
-    user = User.new(name: 'test_user')
+  it "does not validate user if name already exists" do
+    User.create!(name: 'user1')
+    user = User.new(name: 'user1')
     expect(user.valid?).to be false
   end
 end
