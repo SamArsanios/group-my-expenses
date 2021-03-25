3a5669679a21a09b6dfae492fa9652f3900a958a
SamArsanios <samson.kibrom@hotmail.com>
2021-03-26 00:42:44 +0300

    Remove logout logic from view and add a helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,12 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def logout_link
+    content = ''
+    if logged_in?
+      content << link_to('Logout', logout_path, method: 'delete', class: 'nav-link')
+    end
+    content.html_safe
+  end
 end
