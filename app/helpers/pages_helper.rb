0382ec7e6bf16bcbd5fc9995bface759e97cb8e9
SamArsanios <samson.kibrom@hotmail.com>
2021-03-25 14:25:13 +0300

    Add extra dropdown box to test for multiselct

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+    def navbar_dropdown_items
+        result = ''
+        if current_user
+          result << (link_to 'Main page', root_path, class: 'dropdown-item')
+          result << (link_to 'Sign out', logout_path, method: :delete, class: 'dropdown-item')
+        else
+          result << (link_to 'Sign in', login_path, class: 'dropdown-item')
+          result << (link_to 'Sign up', signup_path, class: 'dropdown-item')
+        end
+        result.html_safe
+      end
+    
 end
