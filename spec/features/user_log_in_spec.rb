4c04738fc3aef07bfb70adc5d8ef5f1658882668
SamArsanios <samson.kibrom@hotmail.com>
2021-03-27 11:17:25 +0300

    Add integration test for user login

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -6,23 +6,21 @@ RSpec.describe 'UserSignIns', type: :system do
   end
 
   it 'signs in valid user' do
-    user = User.create(name: 'test_user', password: '123456')
+    user = User.create(name: 'user1')
     visit root_url
     click_link_or_button 'Login'
     fill_in 'session[name]', with: user.name
-    fill_in 'session[password]', with: '123456'
     click_link_or_button 'Login'
     expect(page).to have_css('img.avatar', count: 1)
   end
   it 'signs out logged in user' do
-    user = User.create(name: 'test_user', password: '123456')
+    user = User.create(name: 'user1')
     visit root_url
     click_link_or_button 'Login'
     fill_in 'session[name]', with: user.name
-    fill_in 'session[password]', with: '123456'
     click_link_or_button 'Login'
     expect(page).to have_css('img.avatar', count: 1)
     click_link_or_button 'Logout'
-    expect(page).to have_css('h1.home-title', count: 1, text: 'Group Payments')
+    expect(page).to have_css('h1.home-heading', count: 1, text: 'Group My Expenses')
   end
 end
